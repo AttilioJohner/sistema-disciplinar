644a7afa41d06f64591cc25b434fde835e1d6f3a
AttilioJohner <ajtachinardi@gmail.com>
2025-08-16 22:42:00 -0400

    Fix navigation paths and clean auth guard

--- assets/js/auth-guard.js
+++ assets/js/auth-guard.js
@@ -55,36 +55,3 @@
     return new Promise((resolve) => onReady(resolve));
   };
 })();
-// assets/js/auth-guard.js
-(function () {
-  function isReady() {
-    return (typeof firebase !== 'undefined')
-      && firebase.apps && firebase.apps.length
-      && typeof firebase.auth === 'function';
-  }
-  function onReady(cb) {
-    if (isReady()) return cb();
-    const t = setInterval(() => { if (isReady()) { clearInterval(t); cb(); } }, 100);
-  }
-
-  window.requireAuth = function requireAuth(opts = {}) {
-    const loginPath = opts.loginPath || 'pages/login.html';
-    const onAuth = typeof opts.onAuth === 'function' ? opts.onAuth : function () {};
-
-    onReady(function () {
-      firebase.auth().onAuthStateChanged(function (user) {
-        if (!user) window.location.href = loginPath;
-        else onAuth(user);
-      });
-    });
-  };
-
-  window.logout = async function logout(redirectTo) {
-    await firebase.auth().signOut();
-    if (redirectTo) window.location.href = redirectTo;
-  };
-
-  window.waitForAuthReady = function waitForAuthReady() {
-    return new Promise((resolve) => onReady(resolve));
-  };
-})();
